1e6d617c9ee249adb41297b24aada467ae842d7e
pavan_dudla <pavan.dudla1@gmail.com>
2024-10-17 12:44:37 +0530

    removed underscore in between forgotpassword

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path='/contactus' element={<ContactUs/>}/>
         <Route path='/counter' element={<CounterSection/>}/>
-        <Route path='/forgot_password' element={<ForgotPassword/>}/>
+        <Route path='/forgotPassword' element={<ForgotPassword/>}/>
       </Routes>
       <Footer />
     </Router>
